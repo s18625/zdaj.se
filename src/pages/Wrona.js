36df668f5a9272437583fb2480c65d182a063d47
Stefan <s18625@gmail.com>
2021-05-03 18:28:32 +0200

    com

--- src/pages/Wrona.js
+++ src/pages/Wrona.js
@@ -1,23 +1,43 @@
 import "./Wrona.css";
-import React, { useRef, useEffect } from "react";
+import React, {
+  useRef,
+  useEffect
+} from "react";
 import gsap from "gsap";
 import logo from "../images/logo.png";
 const Wrona = () => {
   useEffect(() => {
-    const t1 = gsap.timeline({ defaults: { ease: "power4.inOut" } });
-    t1.to(".text", { opacity: "1", duration: 1, stagger: 0.3 });
-    t1.to(".up", { y: "-100%", duration: 2 });
+    const t1 = gsap.timeline({
+      defaults: {
+        ease: "power4.inOut"
+      }
+    });
+    t1.to(".text", {
+      opacity: "1",
+      duration: 1,
+      stagger: 0.3
+    });
+    t1.to(".up", {
+      y: "-100%",
+      duration: 2
+    });
     // t1.to(".wrona", { y: "-100%", duration: 1 });
   });
 
-  return (
-    <div className="wrona up">
-      <span className="text">Wrona</span>
-      <span className="text">ty</span>
-      <span className="text">kurwo</span>
-      <span className="text">pierdolona</span>
-      <img className="logo up" src={logo} alt="" />
-    </div>
+  return ( <
+    div className = "wrona up" >
+    <
+    span className = "text" > Koniec < /span> <
+    span className = "text" > z placeniem < /span> <
+    span className = "text" > za < /span> <
+    span className = "text" > ITN < /span> <
+    img className = "logo up"
+    src = {
+      logo
+    }
+    alt = "" / >
+    <
+    /div>
   );
 };
 
